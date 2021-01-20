d4e636038baa4ce1d6ab31f276586f55dbd2b6b4
mcau92 <michael.cauduro.dev@gmail.com>
2021-01-20 18:29:56 +0100

    dev

--- com.cauduro.performer.nandi/src/main/java/com/cauduro/performer/nandi/models/UserModel.java
+++ com.cauduro.performer.nandi/src/main/java/com/cauduro/performer/nandi/models/UserModel.java
@@ -1,26 +1,17 @@
 package com.cauduro.performer.nandi.models;
 
 import java.time.LocalDate;
-import java.util.UUID;
-
 import lombok.Getter;
-import lombok.NonNull;
 import lombok.Setter;
 
 @Getter
 @Setter
 public class UserModel {
 
-	
-	private String userId;
-	@NonNull
-	private String name;
-	@NonNull
-	private String surname;
-	private String cf;
-	private LocalDate dateOfBirth;
-	private String placeOfBirth;
-	
-	
-	
+  private String userId;
+  private String name;
+  private String surname;
+  private String cf;
+  private LocalDate dateOfBirth;
+  private String placeOfBirth;
 }
