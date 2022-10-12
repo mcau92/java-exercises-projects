aef485b902ae43539a0d609f930dab90febf067e
Michael <michael.cauduro.dev@gmail.com>
2022-10-12 14:03:47 +0200

    Delete com.cauduro.performer.nandi directory

--- com.cauduro.performer.nandi/src/main/java/com/cauduro/performer/nandi/repository/UserRepository.java
+++
@@ -1,6 +0,0 @@
-package com.cauduro.performer.nandi.repository;
-
-import org.springframework.data.jpa.repository.JpaRepository;
-import com.cauduro.performer.nandi.entity.UserEntity;
-
-public interface UserRepository extends JpaRepository<UserEntity, Integer> {}
